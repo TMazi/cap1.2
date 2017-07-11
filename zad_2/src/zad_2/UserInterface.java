74a1f99c2b5b434cfd2d336ffdc5f60df7621ccd
TMazi <tomasz.maz94@gmail.com>
2017-07-11 14:01:06 +0200

    LittleChagnes

--- zad_2/src/zad_2/UserInterface.java
+++ zad_2/src/zad_2/UserInterface.java
@@ -15,34 +15,27 @@ public class UserInterface {
 		List<Person> participians;
 		LoadSavePeople lsp;
 
-		System.out.println(
-				"Podaj sciezke do zapisu plikow lub nacisnij enter, aby uzyc domyslnej: " + defaultSaveFilePath);
-		String path = scan.nextLine();
+		lsp = new LoadSavePeople(defaultSaveFilePath);
+		participians = lsp.loadData();
 
-		try {
+		while (!finished) {
 
-			if (path.length() < 1)
-				lsp = new LoadSavePeople(defaultSaveFilePath);
-			else
-				lsp = new LoadSavePeople(path);
-			participians = lsp.loadData();
-
-			while (!finished) {
+			try {
 				System.out.println("Aby wybrac osoby o nazwiskach na dana litere, wpisz ja i potwierdz enterem");
 				System.out.println("Aby podzielic ludzi na grupy o danej wielkosci, wpisz liczbe i potwierdz enterem");
 				input = scan.nextLine();
 
 				Checker check = new Checker(input, participians, participians.size());
 				if (check.processPeople()) {
-					System.out.println("Pomyslnie zapisano do pliku");
+					System.out.println("Pomyslnie zapisano do pliku. Sciezka " + defaultSaveFilePath);
 					finished = true;
 				}
 			}
-		}
 
-		catch (RuntimeException e) {
-			System.out.println(e.getMessage());
-			finished = false;
+			catch (RuntimeException e) {
+				System.out.println(e.getMessage());
+				finished = false;
+			}
 		}
 	}
 
